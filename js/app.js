a84fd4399e067fbe4d53005963fb396175d84e38
jessica <jessicadaianaandre@gmail.com>
2023-12-02 13:37:54 -0300

    Header con JavaScript y full responsive

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  //selección de elementos DOM
+  const toggleButton = document.querySelector(".navbar__toogle-btn");
+  const mobileMenu = document.querySelector(".navbar__mobile-menu");
+  // Si el menú está oculto ('none' o vacio), lo muestra cambiando a 'flex'.
+  //Si el menú movil ya está visible ('flex'), lo oculta cambiando a 'none'.
+
+  const toggleMenu = () => {
+    mobileMenu.style.display =
+      mobileMenu.style.display === "none" || mobileMenu.style.display === ""
+        ? "flex"
+        : "none";
+  };
+  //funcion para ocultar ell menu despegable
+  const hideMenuResize = () => {
+    mobileMenu.style.display = "none"
+  }
+  //evente listener o eventos de escucha
+  toggleButton.addEventListener("click", toggleMenu);
+  window.addEventListener("resize", hideMenuResize);
+window.addEventListener("load", hideMenuResize);
+});
